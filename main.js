45601f260f4325c2a9ae0749dad1052741194e74
Mikko Pohja <mjpohja@gmail.com>
2016-06-06 22:53:00 +0300

    Multi update websocket proto

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const server = require('./lib/server');
 const scheduler = require('./lib/scheduler')
 const fs = require('fs');
 const path = require('path');
+const _ = require('lodash');
 
 const dataSourcePath = './datasource';
 
@@ -19,3 +20,30 @@ fs.readdirSync(dataSourcePath).forEach(function(file) {
 
 
 server.start(3000);
+
+
+
+
+// Testing websockets
+const WebSockerServer = require('ws').Server;
+const ws = new WebSockerServer({port: 7000});
+
+ws.on('connection', socket => {
+    socket.on('message', msg => {
+        console.log(`Socket message received ${msg}`);
+    });
+
+    let updater = setInterval(() => {
+        var updatedData = _.range(Math.floor(Math.random() * 5)).map(i => {
+            return {
+                id: i,
+                data: (18 + Math.random() * 4).toFixed(2)
+            };
+        });
+        socket.send(JSON.stringify({
+            updatedData
+        }));
+    }, 1000);
+
+    socket.on('close', () => clearInterval(updater));
+})
