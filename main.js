ce5cd98af32b52806dd6bb9f7c58b71a99d8b6ab
Mikko Pohja <mikko.pohja@futurice.com>
2016-10-29 23:14:14 +0300

    Disabled socket connection for now

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 'use strict';
 
 const server = require('./lib/server');
-const scheduler = require('./lib/scheduler')
-const fs = require('fs');
-const path = require('path');
 const _ = require('lodash');
 
 const routes = require('./lib/routes');
 
 server.start(3000, routes);
+// server.startSocket();
 
+/*
 // Testing websockets
 const WebSockerServer = require('ws').Server;
 const ws = new WebSockerServer({port: 7000});
@@ -33,3 +32,4 @@ ws.on('connection', socket => {
 
     socket.on('close', () => clearInterval(updater));
 })
+*/
