a88ac08e5fd196351d48769b32735e513fac43a0
Mikko Pohja <mikko.pohja@futurice.com>
2017-08-24 23:29:12 +0300

    Database table for storing forecast values

--- migrations/20170213145414_userinfo.js
+++ migrations/20170213145414_userinfo.js
@@ -7,6 +7,15 @@ exports.up = function(knex, Promise) {
         knex.schema.alterTable('measurements', function(table) {
             table.timestamp('measurement_time').defaultTo(knex.fn.now());
         }),
+        knex.schema.createTable('forecast', function(table) {
+            table.increments('id').notNullable();
+            table.float('value').notNullable();
+            table.integer('distance').notNullable();
+            table.string('step').notNullable().defaultsTo('hour');
+            table.timestamps(true, true);
+            table.integer('sensor_id');
+            table.foreign('sensor_id').references('sensors.id');
+        })
     ]);
 
 };
@@ -18,6 +27,7 @@ exports.down = function(knex, Promise) {
         }),
         knex.schema.alterTable('measurements', function(table) {
             table.dropColumn('measurement_time');
-        })
+        }),
+        knex.schema.dropTableIfExists('forecast')
     ]);
 };
